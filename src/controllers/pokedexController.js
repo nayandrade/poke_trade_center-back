5b534f7b1615c291a65c9dd8c08d1b99426ce8f2
Nayara Dalla Vecchia <nrdeandrade@gmail.com>
2022-10-06 01:41:44 -0300

    feat: search all my pokemons

---
+++ src/controllers/pokedexController.js
@@ -0,0 +1,7 @@
+import * as pokedexServices from "../services/pokedexServices.js";
+
+export async function getMyPokedex(req, res) {
+  const { id } = res.locals.userData;
+  const pokemons = await pokedexServices.getMyPokedex(id);
+  res.status(200).send(pokemons);
+}
